3d3f68b823225ec4f02f5111e52c61caecb931d7
EddyPERREUL <e.perreul@gmail.com>
2020-07-10 13:59:49 +0200

    ex01

--- ex04- /script.js
+++ ex04- /script.js
@@ -1,8 +1,17 @@
 "use strict"
 /* demander confirmation 'Confirmez-vous d'être majeur ?' 
 et affecter la réponse à la variable answer */
+const answer = confirm(`Confirmez-vous d'être majeur ?`)
+console.log(answer)
 /* créer la variable message
 affecter-lui 'Bienvenue !' si la réponse et positive ou 
 'Vous n'être pas autorisé !' dans le cas contraire */
+let message 
+if (answer) {
+    message = 'Bienvenue !'
+} else {
+    message = `Vous n'êtes pas autorisé !`
+}
+console.log(message)
 /* Afficher le message */
 /* 2 solutions avec if et avec l'opérateur conditionnel (ternary operator) */
